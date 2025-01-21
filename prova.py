d60b7d722e21afe00e3045ca035d6cff4d0f38cd
teago777 <teagomatias@gmail.com>
2025-01-21 12:24:16 -0300

    Update prova.py
    explicação sobre o código

--- prova.py
+++ prova.py
@@ -3,3 +3,5 @@ altura = float(input("A Altura do Retângulo: "))
 area = base * altura
 
 print ("A Área do retângulo é: ", area)
+
+// Programa para calcular área de um retângulo, pedindo informações ao usuário (base e altura) e depois entregando o resultado da conta Área = Base x Altura.
